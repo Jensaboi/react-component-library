e5d829c2093c79cc84431e2c7dd487ea80a8425e
Jensaboi <107625279+Jensaboi@users.noreply.github.com>
2025-07-12 14:51:17 +0200

    added the basic tab functionality

--- src/pages/ComponentsPage.jsx
+++ src/pages/ComponentsPage.jsx
@@ -17,7 +17,16 @@ export default function ComponentsPage(){
         <main>
             <Header />
             <section>
-
+                <h2>Components</h2>
+                <p></p>
+                <nav>
+                    {
+                        allTabs.map((tab, index) => (
+                            <button key={tab.label} onClick={()=> setCurrentTab(index)}>{tab.label}</button>
+                        ))
+                    }
+                </nav>
+                    {allTabs[tab].tab}
             </section>
         </main>
     )
