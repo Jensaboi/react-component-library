8028b2e197452a126603004ce34b707e1392d376
Jensaboi <107625279+Jensaboi@users.noreply.github.com>
2025-07-13 07:26:39 +0200

    Update SearchModal.jsx

--- src/components/ui/SearchModal.jsx
+++ src/components/ui/SearchModal.jsx
@@ -16,17 +16,18 @@ export default function SearchModal(){
             </button>
 
             { open && (
-                <div className="search-modal">
-                    <div className="search-modal-content-container">
-                        <button onClick={toggleOpen} className="button-reset">
-                            <IoCloseOutline className="utility-icons"  size="26px" />
-                        </button>
-                        <form>
+                <div className="fixed w-full bg-white text-black dark:bg-slate-900 dark:text-white top-0 left-0 h-screen flex flex-col justify-start items-center">
+                    <div className="flex flex-row justify-start items-start">
+                        <form className="flex flex-row justify-center items-center">
                             <label htmlFor="query">
                                 <IoMdSearch className="utility-icons" size="20px"/>
                             </label>
                             <input name="query" id="query" type="text"/>
                         </form>
+                        
+                        <button onClick={toggleOpen}>
+                            <IoCloseOutline className="utility-icons"  size="26px" />
+                        </button>
                     </div>
                 </div>
             )}
