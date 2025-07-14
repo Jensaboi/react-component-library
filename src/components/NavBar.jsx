d2fac2953d914558042935dda860721663a821ed
Jensaboi <107625279+Jensaboi@users.noreply.github.com>
2025-07-14 10:05:46 +0200

    added link path in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,9 @@ export default function Navbar({ ...rest }) {
     return (
         <nav {...rest}>
             <ul className="flex flex-col gap-1 justify-start items-start md:flex-row md:justify-center md:items-center">
-                <NavListLinkItem>Getting Started</NavListLinkItem>
+                <NavListLinkItem to="/Get started">
+                    Getting Started
+                </NavListLinkItem>
                 <NavListLinkItem to="/Components">Components</NavListLinkItem>
                 <NavListLinkItem>Documents</NavListLinkItem>
                 <NavListLinkItem>Github</NavListLinkItem>
