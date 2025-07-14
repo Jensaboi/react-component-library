8fdbb8b6dab70f070fd8bb45e1635e0b89af3dd1
Jensaboi <107625279+Jensaboi@users.noreply.github.com>
2025-07-14 19:14:25 +0200

    asd

--- src/components/tabs/BannersTab.jsx
+++ src/components/tabs/BannersTab.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Banner from "../ui/Banner";
 
 export default function BannersTab() {
-  return (
-    <>
-        
-    </>
-  );
+    return (
+        <>
+            <Banner size="100%" />
+        </>
+    );
 }
--- src/components/ui/Banner.jsx
+++ src/components/ui/Banner.jsx
@@ -1,41 +1,60 @@
-import checkCircle from '../assets/check-circle.svg'
-import exclamationTriangle from "../assets/exclamation-triangle.svg"
-import infoCircle from "../assets/info-circle.svg"
-import xCircle from "../assets/x-circle.svg"
+import checkCircle from "../../assets/check-circle.svg";
+import exclamationTriangle from "../../assets/exclamation-triangle.svg";
+import infoCircle from "../../assets/info-circle.svg";
+import xCircle from "../../assets/x-circle.svg";
 
-export default function Banner({ size = '896px', children, variant = 'neutral', title}){
+export default function Banner({
+    size = "896px",
+    children,
+    variant = "neutral",
+    title = null,
+    icon = null,
+}) {
+    if (!icon && variant === "success") {
+        icon = checkCircle;
+    }
+    if (!icon && variant === "warning") {
+        icon = exclamationTriangle;
+    }
+    if (!icon && variant === "error") {
+        icon = xCircle;
+    }
+    if (!icon && variant === "neutral") {
+        icon = infoCircle;
+    }
 
-let icon;
-if(variant === 'success'){
-    icon = checkCircle;
-}else if(variant === 'warning'){
-    icon = exclamationTriangle;
-}else if(variant === 'error'){
-    icon = xCircle
-}else{
-    icon = infoCircle;
-}
+    if (!title && variant === "success") {
+        title = "Congratulations";
+    }
+    if (!title && variant === "warning") {
+        title = "Attention";
+    }
+    if (!title && variant === "error") {
+        title = "There is a problem with your application";
+    }
+    if (!title && variant === "neutral") {
+        title = "Update availbe";
+    }
 
-if(!title && variant === 'success'){
-    title = 'Congratulations'
-}
-if(!title && variant === 'warning'){
-    title = 'Attention'
-}
-if(!title && variant === 'error'){
-    title = 'There is a problem with your application'
-}
-if(!title && variant === 'neutral'){
-    title = 'Update availbe'
-}
-
-    return(
-        <div className={variant} style={{width: size, maxWidth: '896px', display: 'flex', justifyContent: 'start', alignItems: 'center', padding: '16px', borderRadius: '6px', gap: '10px'}}>
-            <img style={{alignSelf: 'start'}} src={icon}/>
+    return (
+        <div
+            className={variant}
+            style={{
+                width: size,
+                maxWidth: "896px",
+                display: "flex",
+                justifyContent: "start",
+                alignItems: "center",
+                padding: "16px",
+                borderRadius: "6px",
+                gap: "10px",
+            }}
+        >
+            <img style={{ alignSelf: "start" }} src={icon} />
             <div className={variant}>
                 <h3>{title}</h3>
                 {children && <p>{children}</p>}
             </div>
         </div>
-    )
+    );
 }
