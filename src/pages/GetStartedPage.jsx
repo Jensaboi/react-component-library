dbe355547f241315852861e0ae5da2e219e8921e
Jensaboi <107625279+Jensaboi@users.noreply.github.com>
2025-07-14 18:16:53 +0200

    added wrapper to all pages

--- src/pages/GetStartedPage.jsx
+++ src/pages/GetStartedPage.jsx
@@ -3,10 +3,12 @@ import Header from "../components/Header";
 export default function GetStartedPage() {
     return (
         <>
-            <Header />
-            <main className="">
-                <h1>Get started</h1>
-            </main>
+            <div className="bg-light-primary-100 flex flex-col h-screen w-full gap-10">
+                <Header />
+                <main className="flex-1 w-full">
+                    <h1>Get started</h1>
+                </main>
+            </div>
         </>
     );
 }
