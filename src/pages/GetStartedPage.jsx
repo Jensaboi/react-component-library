bb3344e694b55477e8416d1620d89c191fbedd05
Jensaboi <107625279+Jensaboi@users.noreply.github.com>
2025-07-14 10:04:51 +0200

    create GetStartedPage

---
+++ src/pages/GetStartedPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+export default function GetStartedPage() {
+    return (
+        <>
+            <Header />
+            <main>
+                <h1>Get started</h1>
+            </main>
+        </>
+    );
+}
