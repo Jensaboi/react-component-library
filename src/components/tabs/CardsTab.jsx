1bfb103df09657a01691ce0656eb6cdd52410c3c
Jens <107625279+Jensaboi@users.noreply.github.com>
2025-07-14 22:33:33 +0200

    create card

--- src/components/tabs/CardsTab.jsx
+++ src/components/tabs/CardsTab.jsx
@@ -3,8 +3,10 @@ export default function CardsTab() {
   return (
     <>
       <Card>
-        <h2>card title</h2>
-        <p>
+        <h2 className="text-light-text-primary text-xl mb-6">
+          Easy Deployment
+        </h2>
+        <p className="text-md">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla,
           doloremque dolorem. Nesciunt dicta illum animi!
         </p>
--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,8 +1,8 @@
 import deployMentSvg from "../../assets/deployment.svg";
-export default function Card({ children, ...rest }) {
+export default function Card({ children }) {
   return (
-    <div {...rest}>
-      <div>
+    <div className="bg-light-primary w-100 text-light-text-100 font-medium p-4 pt-15 hover:shadow-lg shadow-sm text-center rounded-lg relative">
+      <div className="bg-blue-500 absolute -top-5 left-[44%] w-fit py-3 px-3 rounded-md">
         <img src={deployMentSvg} />
       </div>
       {children}
