150a38608811d686eee13e7d1a3dd2a7b6eff831
Jensaboi <107625279+Jensaboi@users.noreply.github.com>
2025-07-14 21:49:14 +0200

    Update Banner.jsx

--- src/components/ui/Banner.jsx
+++ src/components/ui/Banner.jsx
@@ -4,60 +4,62 @@ import infoCircle from "../../assets/info-circle.svg";
 import xCircle from "../../assets/x-circle.svg";
 
 export default function Banner({
-  size = "896px",
-  children,
-  status = "neutral",
-  title = null,
-  icon = null,
+    size = "896px",
+    children,
+    status = "neutral",
+    title = null,
+    icon = null,
 }) {
-  if (!icon && status === "success") {
-    icon = checkCircle;
-  }
-  if (!icon && status === "warning") {
-    icon = exclamationTriangle;
-  }
-  if (!icon && status === "error") {
-    icon = xCircle;
-  }
-  if (!icon && status === "neutral") {
-    icon = infoCircle;
-  }
+    if (!icon && status === "success") {
+        icon = checkCircle;
+    }
+    if (!icon && status === "warning") {
+        icon = exclamationTriangle;
+    }
+    if (!icon && status === "error") {
+        icon = xCircle;
+    }
+    if (!icon && status === "neutral") {
+        icon = infoCircle;
+    }
 
-  if (!title && status === "success") {
-    title = "Congratulations";
-  }
-  if (!title && status === "warning") {
-    title = "Attention";
-  }
-  if (!title && status === "error") {
-    title = "There is a problem with your application";
-  }
-  if (!title && status === "neutral") {
-    title = "Update availbe";
-  }
+    if (!title && status === "success") {
+        title = "Congratulations";
+    }
+    if (!title && status === "warning") {
+        title = "Attention";
+    }
+    if (!title && status === "error") {
+        title = "There is a problem with your application";
+    }
+    if (!title && status === "neutral") {
+        title = "Update availbe";
+    }
 
-  const defaultStyles =
-    "m-w-[896px] w-fit min-w-[360px] font-sm gap-3 p-[16px] rounded-md";
+    const defaultStyles =
+        "m-w-[896px] w-fit min-w-[360px] font-sm gap-3 p-[16px] rounded-md";
 
-  const statuss = {
-    neutral: "bg-blue-100 text-blue-500",
-    success: "bg-green-100 text-green-500",
-    warning: "bg-yellow-100 text-yellow-600",
-    error: "bg-red-100 text-red-500",
-  };
-  const titleColors = {
-    neutral: "text-blue-800",
-    success: "text-green-800",
-    warning: "text-yellow-800",
-    error: "text-red-800",
-  };
-  return (
-    <div className={`${defaultStyles} ${statuss[status]} `}>
-      <div className="flex flex-row justify-start items-center gap-2">
-        <img className="w-4" src={icon} />
-        <h3 className={`font-medium ${titleColors[status]}`}>{title}</h3>
-      </div>
-      {children && <p className="font-normal ml-6">{children}</p>}
-    </div>
-  );
+    const statuss = {
+        neutral: "bg-blue-100 text-blue-500",
+        success: "bg-green-100 text-green-500",
+        warning: "bg-yellow-100 text-yellow-600",
+        error: "bg-red-100 text-red-500",
+    };
+    const titleColors = {
+        neutral: "text-blue-800",
+        success: "text-green-800",
+        warning: "text-yellow-800",
+        error: "text-red-800",
+    };
+    return (
+        <div className={`${defaultStyles} ${statuss[status]} `}>
+            <div className="flex flex-row justify-start items-center gap-2">
+                <img className="w-4" src={icon} />
+                <h3 className={`font-medium ${titleColors[status]}`}>
+                    {title}
+                </h3>
+            </div>
+            {children && <p className="font-normal ml-6">{children}</p>}
+        </div>
+    );
 }
