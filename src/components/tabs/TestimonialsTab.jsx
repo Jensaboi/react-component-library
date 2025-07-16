389f7b5a582f2c37f1399b8b8d40feedfe6fef75
Jens <107625279+Jensaboi@users.noreply.github.com>
2025-07-16 03:35:09 +0200

    Testimonial component with image complete

--- src/components/tabs/TestimonialsTab.jsx
+++ src/components/tabs/TestimonialsTab.jsx
@@ -1,8 +1,14 @@
 import TestimonialWithImg from "../ui/TestimonialWithImg";
+import TestimonialImg from "../../assets/testimonial-img.jpg";
+
 export default function TestimonialsTab() {
-    return (
-        <>
-            <TestimonialWithImg />
-        </>
-    );
+  return (
+    <>
+      <TestimonialWithImg img={TestimonialImg} name={"May Andersons"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+        praesentium dignissimos dolorem dolorum quisquam amet nesciunt. Mollitia
+        quibusdam nostrum voluptate corrupti culpa!
+      </TestimonialWithImg>
+    </>
+  );
 }
--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,7 +1,7 @@
 import deployMentSvg from "../../assets/deployment.svg";
 export default function Card({ children }) {
   return (
-    <div className="bg-light-primary w-100 text-light-text-100 font-medium p-4 pt-15 hover:shadow-lg shadow-sm text-center rounded-lg relative">
+    <div className="bg-light-primary max-w-100 text-light-text-100 font-medium p-4 pt-15 hover:shadow-lg hover:cursor-pointer shadow-sm text-center rounded-lg relative">
       <div className="bg-blue-500 absolute -top-5 left-[44%] w-fit py-3 px-3 rounded-md">
         <img src={deployMentSvg} />
       </div>
